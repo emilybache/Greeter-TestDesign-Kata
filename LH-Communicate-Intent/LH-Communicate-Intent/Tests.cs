620b1b2b6fbcb36d2d57bbf4520125fa456411e6
xavi ametller <xavi.ametller@gmail.com>
2025-09-22 00:40:18 +0200

    Add project, solution and test files... 😅

---
+++ LH-Communicate-Intent/LH-Communicate-Intent/Tests.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+
+namespace LH_Communicate_Intent;
+
+public class Tests
+{
+    [Fact]
+    public void GoodMorning()
+    {
+        var mock = new Mock<IDateTimeProvider>();
+        mock.Setup(dtp => dtp.GetDateTime()).Returns(new DateTime(2000, 1, 1, 7, 0,0 ));
+        var greeter = new Greeter(mock.Object);
+        
+        Assert.Equal("Good morning Arnau", greeter.Greet("Arnau"));
+    }
+    
+    [Fact]
+    public void InTheAfternoon()
+    {
+        var mock = new Mock<IDateTimeProvider>();
+        mock.Setup(dtp => dtp.GetDateTime()).Returns(new DateTime(2000, 1, 1, 16, 0,0 ));
+        var greeter = new Greeter(mock.Object);
+        
+        Assert.Equal("Good afternoon Arnau", greeter.Greet("Arnau"));
+    }
+    
+    [Fact]
+    public void AfterTheAfternoon()
+    {
+        var mock = new Mock<IDateTimeProvider>();
+        mock.Setup(dtp => dtp.GetDateTime()).Returns(new DateTime(2000, 1, 1, 0, 0,0 ));
+        var greeter = new Greeter(mock.Object);
+        
+        Assert.Equal("Good night Arnau", greeter.Greet("Arnau"));
+    }
+}
