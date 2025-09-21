d013ab6249c4dd1c759b615d2a16c64df0b892e9
xavi ametller <xavi.ametller@gmail.com>
2025-09-22 00:21:49 +0200

    2nd project

---
+++ LH-Communicate-Intent/LH-Communicate-Intent/Greeter.cs
@@ -0,0 +1,30 @@
+namespace LH_Communicate_Intent;
+
+internal class Greeter
+{
+    private readonly IDateTimeProvider _datetimeProvider;
+
+    public Greeter(IDateTimeProvider datetimeProvider)
+    {
+        _datetimeProvider = datetimeProvider;
+    }
+    
+    public string Greet(string name)
+    {
+        var hour = _datetimeProvider.GetDateTime().Hour;
+        string template = string.Empty;
+        if (hour.IsBetween(7, 13))
+        {
+            template = "Good morning {0}";
+        }
+        if (hour.IsBetween(14,19))
+        {
+            template = "Good afternoon {0}";
+        }
+        if (hour.IsBetween(20, 6))
+        {
+            template = "Good night {0}";
+        }
+        return String.Format(template, name);
+    }
+}
---
+++ LH-Communicate-Intent/LH-Communicate-Intent/IDateTimeProvider.cs
@@ -0,0 +1,6 @@
+namespace LH_Communicate_Intent;
+
+public interface IDateTimeProvider
+{
+    DateTime GetDateTime();
+}
